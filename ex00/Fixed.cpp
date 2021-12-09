2694e316c5615f8bae26dc710dca0f0a58c380c1
Nathan Geschwind <ngeschwi@z3r6p1.42lyon.fr>
2021-12-09 14:43:02 +0100

    debut

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,13 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	std::cout << "Default constructor called" << std::endl;
+	return ;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+	return ;
+}
---
+++ ex00/Fixed.hpp
@@ -0,0 +1,16 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <string>
+# include <stdio.h>
+# include <stdlib.h>
+
+class Fixed
+{
+	private:
+	public:
+		Fixed();
+		~Fixed();
+};
+
+#endif
